e3e6c92ea47977ecee2328fff930ee77fdfc56ff
Masumi Yano <92832478+MasumiYano@users.noreply.github.com>
2024-09-24 20:08:10 -0700

    initial commit

---
+++ lib/syncup.ex
@@ -0,0 +1,18 @@
+defmodule Syncup do
+  @moduledoc """
+  Documentation for `Syncup`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Syncup.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/syncup_test.exs
@@ -0,0 +1,8 @@
+defmodule SyncupTest do
+  use ExUnit.Case
+  doctest Syncup
+
+  test "greets the world" do
+    assert Syncup.hello() == :world
+  end
+end
